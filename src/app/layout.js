4c3df0f91896baa66d9b988a1efb545991082d52
mdsamiulislam54 <samiulm5332@gmail.com>
2025-07-21 11:46:12 +0600

    basic font optimization setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
+import { Poppins} from "next/font/google";
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+})
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={poppins.className}
       >
         <Navbar/>
        <main>
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
   return (
   <div className="container mx-auto p-4">
     <h1>Welcome to My App</h1>
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
     <Image placeholder="blur" src={Thum} alt="thum"/>
 
   </div>
